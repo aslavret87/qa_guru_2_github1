2594197f6ed1c20f799455cc24f3c8a90ee63e3c
aslavret87 <aslavret87@gmail.com>
2022-11-13 00:10:40 +0300

    init commit from console

---
+++ qa_guru_2_github/src/main/java/org/example/ExampleTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import static org.testng.asserts.Assertion.*;
+
+public class ExampleTest {
+
+    @Test
+    void ExampleTest() {
+        Assert.assertTrue(2>1);
+    }
+}
